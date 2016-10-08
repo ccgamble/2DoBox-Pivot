cddc67a94e537f25b79e7371d4673687bfd01b2d
ccgamble <christine.c.gamble@gmail.com>
2016-10-08 09:59:39 -0600

    starting to work on unit tests

--- test/specs/unit-tests.js
+++ test/specs/unit-tests.js
@@ -4,10 +4,19 @@ const domObject = require('../../lib/domObject');
 describe('domObject', function () {
   context('findIdea', function() {
     it('should return object that matches id ', function () {
-      var ideaList = [{id:1}, {name: 'tay', id:2}, {id:3}];
+      var ideaList = [{id:1}, {name: 'Chelsea', id:2}, {id:3}];
       var result = domObject.findIdea(2, ideaList);
-      assert.equal(result.name, 'tay');
+      assert.equal(result.name, 'Chelsea');
     });
   });
+
+  context('removeIdea', function() {
+    it('should remove idea with the given id', function() {
+      var ideaList = [{id:1}, {name: 'Chelsea', id:2}, {id:3}];
+      var result = domObject.removeIdea(2, ideaList);
+      assert.equal(result.length, 2)
+      assert.equal(result, )
+    })
+  })
 
 });
